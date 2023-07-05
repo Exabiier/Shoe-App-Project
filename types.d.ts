b0004416310aadf013506dde1f0a8816f077731e
Exabiier <102698808+Exabiier@users.noreply.github.com>
2023-07-05 16:42:52 -0700

    Basic Home Screen is completed

--- types.d.ts
+++ types.d.ts
@@ -3,3 +3,15 @@ type RootStackParamList = {
     Home: undefined,
 }
 
+//Data types and Interfaces:
+interface Product {
+    id: string;
+    image: string;
+    images: string [];
+    name: string;
+    price: number;
+    sizes: number[];
+    description: string;
+
+}
+
