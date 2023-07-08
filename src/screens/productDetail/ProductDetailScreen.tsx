05636206a71ff9e797db70a32eebfda8834e0ce5
Exabiier <102698808+Exabiier@users.noreply.github.com>
2023-07-07 18:30:38 -0700

    Flatlist shows with product Description

--- src/screens/productDetail/ProductDetailScreen.tsx
+++ src/screens/productDetail/ProductDetailScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image, FlatList, useWindowDimensions, StyleSheet } from 'react-native'
+import { View, Text, Image, FlatList, useWindowDimensions, StyleSheet, ScrollView } from 'react-native'
 import products from '../../data/products'
 
 
@@ -7,32 +7,33 @@ const ProductDetailScreen = () => {
     const { width } = useWindowDimensions();
     
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
         {/* Carassoul for images */}
+        
         <FlatList
         data={product.images}
         renderItem={ ({item}) => (
             // the image gets cut out if aspect ration and flex is use on it 
+        
             <View style={[styles.imageContainer, { width: width}]}>
-                <Image className='h-[100%] w-[100%]' source={{ uri: item }}  style={styles.image} />
+                <Image className='h-[100%] w-[100%]' source={{ uri: item }}  style={[styles.image, { width: width}]} resizeMode="cover" />
             </View>
         )}
         horizontal
         pagingEnabled
         />
-        {/* Title */}
-        <Text>{product.name}</Text>
-        {/* For Price */}
-        <Text>{product.price}</Text>
-        {/* For Description */}
-        <Text>{product.description}</Text>
-        {/* Add Cart button */}
-        <Text></Text>
-        {/* Navigaiton icon */}
-
-      
-
-    </View>
+        <View style={{padding: 20}}>
+            {/* Title */}
+            <Text style={styles.title} >{product.name}</Text>
+            {/* For Price */}
+            <Text style={styles.price}>{product.price}</Text>
+            {/* For Description */}
+            <Text style={styles.description1}>{product.description}</Text>
+            {/* Add Cart button */}
+            <Text></Text>
+            {/* Navigaiton icon */}
+        </View>
+    </ScrollView>
   )
 }
 
@@ -45,9 +46,25 @@ const styles = StyleSheet.create({
         flex: 1,
       },
       image: {
-        
-        // aspectRatio: 1,
+        aspectRatio: 1,
       },
+
+    //  these are the styles for the fonts
+    title: {
+        fontSize: 34,
+        fontWeight: '500',
+        marginVertical: 10,
+    },
+    price: {
+        fontWeight: '500',
+        fontSize: 16,   
+    },
+    description1:{
+        marginVertical: 10,
+        fontSize: 18,
+        lineHeight: 30,
+        fontWeight: '300',
+    }
 })
 
 export default ProductDetailScreen
