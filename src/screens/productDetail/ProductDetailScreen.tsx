37d9f373e7a52108815d4e854040454bdc1596f5
Exabiier <102698808+Exabiier@users.noreply.github.com>
2023-07-14 17:43:52 -0700

    adding the add cart button

--- src/screens/productDetail/ProductDetailScreen.tsx
+++ src/screens/productDetail/ProductDetailScreen.tsx
@@ -1,39 +1,47 @@
-import { View, Text, Image, FlatList, useWindowDimensions, StyleSheet, ScrollView } from 'react-native'
+import { View, Text, Image, FlatList, useWindowDimensions, StyleSheet, ScrollView, Pressable } from 'react-native'
 import products from '../../data/products'
 
 
 const ProductDetailScreen = () => {
     const product = products[0];
     const { width } = useWindowDimensions();
+    const addToCart = () => {
+      console.warn('Add to cart');
+    }
     
   return (
-    <ScrollView style={styles.container}>
-        {/* Carassoul for images */}
-        
-        <FlatList
-        data={product.images}
-        renderItem={ ({item}) => (
-            // the image gets cut out if aspect ration and flex is use on it 
-        
-            <View style={[styles.imageContainer, { width: width}]}>
-                <Image className='h-[100%] w-[100%]' source={{ uri: item }}  style={[styles.image, { width: width}]} resizeMode="cover" />
-            </View>
-        )}
-        horizontal
-        pagingEnabled
-        />
-        <View style={{padding: 20}}>
-            {/* Title */}
-            <Text style={styles.title} >{product.name}</Text>
-            {/* For Price */}
-            <Text style={styles.price}>{product.price}</Text>
-            {/* For Description */}
-            <Text style={styles.description1}>{product.description}</Text>
-            {/* Add Cart button */}
-            <Text></Text>
-            {/* Navigaiton icon */}
-        </View>
-    </ScrollView>
+      <>
+      <ScrollView style={styles.container}>
+          {/* Carassoul for images */}
+          
+          <FlatList
+          data={product.images}
+          renderItem={ ({item}) => (
+              // the image gets cut out if aspect ration and flex is use on it 
+          
+              <View style={[styles.imageContainer, { width: width}]}>
+                  <Image className='h-[100%] w-[100%]' source={{ uri: item }}  style={[styles.image, { width: width}]} resizeMode="cover" />
+              </View>
+          )}
+          horizontal
+          pagingEnabled
+          />
+          <View style={{padding: 20}}>
+              {/* Title */}
+              <Text style={styles.title} >{product.name}</Text>
+              {/* For Price */}
+              <Text style={styles.price}>${product.price}</Text>
+              {/* For Description */}
+              <Text style={styles.description1}>{product.description}</Text>
+              {/* Add Cart button */}
+              <Text></Text>
+              {/* Navigaiton icon */}
+          </View>
+      </ScrollView>
+      <Pressable onPress={() => addToCart()} style={styles.button}>
+        <Text style={styles.buttonText}> Add to cart </Text>
+      </Pressable>
+      </>
   )
 }
 
@@ -57,14 +65,31 @@ const styles = StyleSheet.create({
     },
     price: {
         fontWeight: '500',
-        fontSize: 16,   
+        fontSize: 16,
+        letterSpacing: 1.5,   
     },
     description1:{
         marginVertical: 10,
         fontSize: 18,
         lineHeight: 30,
         fontWeight: '300',
-    }
+    },
+    button: {
+      position: 'absolute',
+      backgroundColor: 'black',
+      bottom: 30,
+      width: '90%',
+      alignSelf: 'center',
+      padding: 20,
+      borderRadius: 100,
+      alignItems: 'center',
+    },
+    buttonText: { 
+      color: 'white',
+      fontSize: 16,
+      fontweight: '500'
+
+    },
 })
 
 export default ProductDetailScreen
